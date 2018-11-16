226dc16b3d5bebe8a540a0c496bbcece69005218
Wei Gao <wei@gaofamily.org>
2018-11-16 14:35:20 -0800

    Customize datanode port. Add compose file to create 1 namenode, 3 datanode locally

--- hadoop/datanode/create_config.sh
+++ hadoop/datanode/create_config.sh
@@ -1,3 +1,3 @@
 if [ ! -f "${HADOOP_CONF_DIR}/hdfs-site.xml" ]; then
-    cp "${HADOOP_CONF_TEMP_DIR}/hdfs-site.xml.tmpl" "${HADOOP_CONF_DIR}/hdfs-site.xml"
+    /bin/sed -e 's|\$HADOOP_DATANODE_ADDRESS|'"${HADOOP_DATANODE_ADDRESS}"'|g' "${HADOOP_CONF_TEMP_DIR}/hdfs-site.xml.tmpl" > "${HADOOP_CONF_DIR}/hdfs-site.xml"
 fi
